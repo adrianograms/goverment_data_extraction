521e207c7f416331c4800528da3f53135d8060b1
adrianograms <adrianograms@hotmail.com>
2024-10-12 11:03:18 -0300

    More improvements to api

--- dags/extract_data.py
+++ dags/extract_data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import requests
 import json
 from datetime import datetime
-from airflow.decorators import dag, task
+from airflow.decorators import dag, task, task_group
 from airflow.models.param import Param
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import input_file_name, substring
@@ -13,6 +13,7 @@ import jaydebeapi
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 page_size = 100
 url_base = "https://api.obrasgov.gestao.gov.br"
 endpoint = "/obrasgov/api/execucao-financeira"
@@ -38,13 +39,14 @@ def generate_url(url_base, endpoint, parameters):
     return url
 
 @task()
-def extract_data_api(url_base, endpoint, year, dest_path, page_size = 100, errors_limit = -1, errors_consecutives_limit = 5, executions_limit = 200):
+def extract_data_api(url_base, endpoint, year, page_size = 100, errors_limit = -1, errors_consecutives_limit = 5, executions_limit = 200):
     success = False
     page = 0
     errors_consecutives = 0
     errors = 0
     executions = 0
     method = "GET"
+    dest_path = os.getenv('PATH_DEST')
     dest_path_file = dest_path + '/' + str(year) + '.json'
 
     Path(dest_path).mkdir(parents=True, exist_ok=True)
@@ -55,17 +57,75 @@ def extract_data_api(url_base, endpoint, year, dest_path, page_size = 100, error
     content_all = []
 
     while success == False and (errors_consecutives < errors_consecutives_limit or errors_consecutives_limit == -1) and (errors < errors_limit or errors_limit == -1) and (executions < executions_limit or executions_limit == -1):
-        url = url_base + endpoint + '?' + 'pagina=' + str(page) + '&' + 'tamanhoDaPagina=' + str(page_size) + '&' + 'anoFinal=' + str(year) + '&' + 'anoInicial=' + str(year) 
+        params = {"pagina": str(page), "tamanhoDaPagina": str(page_size), "anoFinal": str(year), "anoInicial": str(year)}
+        url = generate_url(url_base, endpoint, params)
         response = requests.request(method, url)
         if response.status_code == 200:
             page += 1
             errors_consecutives = 0
             executions += 1
             content_all += response.json()["content"]
-            #with open(dest_path_file, 'w') as f:
-            #    json.dump(response.json()["content"],f)
         elif response.status_code == 404:
             success = True 
+        else:
+            errors_consecutives += 1
+            errors += 1
+            executions += 1
+            if response.status_code == 429:
+                time.sleep(1)
+        print(f'Status Code: {response.status_code}\n'
+            f'Executions: {executions}\n'
+            f'Pages: {page}\n' 
+            f'N° Registers: {len(content_all)}\n'
+            f'Errors: {errors}\n'
+            f'Errors Consecutives: {errors_consecutives}\n')
+        time.sleep(1)
+
+    if success == True:
+        print('Execution Finished with success!')
+    else:
+        print('Execution Finished with error!')
+        if errors_consecutives < errors_consecutives_limit:
+            raise Exception("Number of consecutives errors exceeded")
+        elif errors < errors_limit:
+            raise Exception("Number of total errors exceeded")
+        elif executions < executions_limit:
+            raise Exception("Number of executions exceeded")
+
+    with open(dest_path_file, 'w', encoding='utf-8') as f:
+        json.dump(content_all, f)
+    f.close()
+
+@task()
+def extract_data_api_project(url_base, endpoint, uf, page_size = 100, errors_limit = -1, errors_consecutives_limit = 5, executions_limit = 200):
+    success = False
+    page = 0
+    errors_consecutives = 0
+    errors = 0
+    executions = 0
+    method = "GET"
+    dest_path = os.getenv('PATH_DEST_PROJETO_INVESTIMENTO')
+    dest_path_file = dest_path + '/' + str(uf) + '.json'
+
+    Path(dest_path).mkdir(parents=True, exist_ok=True)
+
+    if os.path.isfile(dest_path_file) == True:
+        os.remove(dest_path_file)
+
+    content_all = []
+
+    while success == False and (errors_consecutives < errors_consecutives_limit or errors_consecutives_limit == -1) and (errors < errors_limit or errors_limit == -1) and (executions < executions_limit or executions_limit == -1):
+        params = {"pagina": str(page), "tamanhoDaPagina": str(page_size), "uf": str(uf)}
+        url = generate_url(url_base, endpoint, params)
+        response = requests.request(method, url)
+        if response.status_code == 200:
+            if(len(response.json()["content"]) == 0):
+                success = True 
+            else:
+                page += 1
+                errors_consecutives = 0
+                executions += 1
+                content_all += response.json()["content"]
         else:
             errors_consecutives += 1
             errors += 1
@@ -102,10 +162,8 @@ def generate_dates(initial_year, final_year):
         years.append(year)
     return years
 
-@task()
-def extract_data_json(year, origin):
-    dotenv_path = Path('/home/adriano/Documentos/airflow/.env')
-    load_dotenv(dotenv_path=dotenv_path)
+@task(max_active_tis_per_dag=1)
+def extract_data_json(year):
     user_dw = os.getenv('USER_DW')
     password_dw = os.getenv('PASSWORD_DW')
     host_dw = os.getenv('HOST_DW')
@@ -113,6 +171,7 @@ def extract_data_json(year, origin):
     port_dw = os.getenv('PORT_DW')
     driver = os.getenv('DRIVER_JDBC_POSTGRES')
     path_jdbc = os.getenv('PATH_JDBC_POSTGRES')
+    origin = os.getenv('PATH_DEST')
     url = f'jdbc:postgresql://{host_dw}:{port_dw}/{database_dw}'
 
     spark = SparkSession\
@@ -134,6 +193,14 @@ def extract_data_json(year, origin):
     mode = 'append'
     properties = {"user": user_dw, "password": password_dw, "driver": driver}
     df.write.jdbc(url=url, table='stg_execucao_financeira', mode=mode, properties=properties)
+
+@task_group()
+def extract_execucao_financeira(url_base, endpoint, initial_year, final_year, page_size, errors_limit, errors_consecutives_limit, executions_limit):
+    years = generate_dates(initial_year, final_year)
+    extract_api = extract_data_api.partial(url_base=url_base, endpoint=endpoint, page_size=page_size,
+                             errors_limit = errors_limit, errors_consecutives_limit = errors_consecutives_limit, executions_limit = executions_limit).expand(year=years)
+    extract_json = extract_data_json.expand(year=years)
+    extract_api >> extract_json
         
 @dag(
     schedule = '@daily',
@@ -144,14 +211,19 @@ def extract_data_json(year, origin):
         "final_year": Param(final_year, type="integer"),
     },
 ) 
-def get_api_data(url_base, endpoint, initial_year, final_year, dest_path, page_size, errors_limit, errors_consecutives_limit, executions_limit):
-    years = generate_dates(initial_year, final_year)
+def get_api_data(initial_year, final_year, page_size, errors_limit, errors_consecutives_limit, executions_limit):
+    #years = generate_dates(initial_year, final_year)
+    url_base = os.getenv('URL_BASE')
+    ufs = ['AC']
 
-    extract_api = extract_data_api.partial(url_base=url_base, endpoint=endpoint, dest_path=dest_path, page_size=page_size,
-                             errors_limit = errors_limit, errors_consecutives_limit = errors_consecutives_limit, executions_limit = executions_limit).expand(year=years)
-    
-    extract_json = extract_data_json.partial(origin=dest_path).expand(year=years)
+    extract_exec_fin = extract_execucao_financeira(url_base, '/obrasgov/api/execucao-financeira', initial_year, final_year, page_size, errors_limit, errors_consecutives_limit, executions_limit)
 
-    extract_api >> extract_json
+    extract_api_project = extract_data_api_project.partial(url_base=url_base, endpoint='/obrasgov/api/projeto-investimento', page_size=page_size,
+                             errors_limit = errors_limit, errors_consecutives_limit = errors_consecutives_limit, executions_limit = executions_limit).expand(uf=ufs)
 
-dag = get_api_data(url_base, endpoint, initial_year, final_year, dest_path, page_size, errors_limit, errors_consecutives_limit, executions_limit)
+    #extract_api = extract_data_api.partial(url_base=url_base, endpoint=endpoint, page_size=page_size,
+    #                         errors_limit = errors_limit, errors_consecutives_limit = errors_consecutives_limit, executions_limit = executions_limit).expand(year=years)
+    #extract_json = extract_data_json.expand(year=years)
+    #extract_api >> extract_json
+
+dag = get_api_data(initial_year, final_year, page_size, errors_limit, errors_consecutives_limit, executions_limit)
